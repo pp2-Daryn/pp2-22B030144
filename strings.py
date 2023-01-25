bd7c9582fcc4048d06baae02e9a32136f4febf2a
pp2-22B030144 <123086496+pp2-Daryn@users.noreply.github.com>
2023-01-25 11:21:51 +0600

    Strings
    https://www.w3schools.com/python/python_strings.asp

---
+++ strings.py
@@ -0,0 +1,18 @@
+print("Hello")
+print('Hello')
+# You can display a string literal with the print() function
+a = "Hello"
+print(a)
+# Assign String to a Variable
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+# You can use three double quotes
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+# Or three single quotes
