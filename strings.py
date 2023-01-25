02a0e073e5ce151a8c7f3351248866e983d40e1b
pp2-22B030144 <123086496+pp2-Daryn@users.noreply.github.com>
2023-01-25 11:32:48 +0600

    Update strings.py

--- strings.py
+++ strings.py
@@ -16,3 +16,31 @@ sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua.'''
 print(a)
 # Or three single quotes
+b = "Hello, World!"
+print(b[2:5])
+# Slicing
+b = "Hello, World!"
+print(b[:5])
+#Slice From the Start
+b = "Hello, World!"
+print(b[2:])
+# Slice To the End
+b = "Hello, World!"
+print(b[-5:-2])
+# Negative Indexing
+# MODIFY STRINGS
+a = "Hello, World!"
+print(a.upper())
+# Upper Case
+a = "Hello, World!"
+print(a.lower())
+# Lower case
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+# Remove Whitespace
+a = "Hello, World!"
+print(a.replace("H", "J"))
+# Replace String
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+# Split strings
